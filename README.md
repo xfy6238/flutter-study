4ac846f319ab77dd745e841828854c7c24861841
fanzkday <17612151214@163.com>
2018-10-11 23:19:56 +0800

    update

--- README.md
+++ README.md
@@ -82,3 +82,63 @@ ListView.builder(
   cacheExtent: 50.0, // 缓冲区的高度
 )
 ```
+
+```dart
+PopupMenuButton<Choice>(
+  onSelected: _select,
+  // icon: Icon(Icons.account_box), // icon 和 child目前二选一
+  child: IconButton(icon: Icon(Icons.access_time), onPressed: null,),
+  initialValue: choices[4], // 默认选中第几项
+  itemBuilder: (BuildContext context) { // 返回PopupMenuItem类型的List<widget>
+    return choices.skip(2).map((Choice choice) {
+      return PopupMenuItem<Choice>(
+        value: choice, // value值
+        child: Text(choice.title),
+        height: 36.0,
+        enabled: false, // 是否可选择
+      );
+    }).toList();
+  },
+)
+```
+
+```dart
+// 可下拉展开的列表
+ExpansionTile(
+  title: Text(root.title), // 标题
+  children: Widget[], // 展开项
+  initiallyExpanded: true, // 是否默认展开
+  trailing: Icon(Icons.rotate_right), // 尾部Widget
+  leading: Icon(Icons.rotate_left), // 头部Widget
+  backgroundColor: Color(Icons.red),
+  onExpansionChanged: (bool b) {},
+)
+```
+
+```dart
+DefaultTabController(
+  length: choices.length,
+  child: Scaffold(
+    appBar: AppBar(
+      title: const Text('Tabbed AppBar'),
+      bottom: TabBar(
+        controller: controller,
+        isScrollable: true, // 是否可左右滚动
+        indicator: new ShapeDecoration(shape: new Border.all(color: Colors.redAccent, width: 1.0)), // 指示器的形状， 默认为下划线
+        indicatorSize: TabBarIndicatorSize.label, // 宽度与label或者tab同宽
+        // 其他属性
+        tabs: <Widget>[
+          Tab(text: "label", icon: Icon(Icons.camera),)
+        ],
+      ),
+    ),
+    body: TabBarView(
+      controller: controller,
+      physics: const AlwaysScrollableScrollPhysics(),
+      children: <Widget>[],
+    ),
+  ),
+)
+// TabBar可以通过DefaultTabController与TabBarView联系在一起
+// 也可以通过与TabBarView的controler公用达到目的
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-import 'components/ListItem.dart';
-
-void main() {
-  debugPaintSizeEnabled = false;
-  runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class TabbedAppBarSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter',
-      theme: new ThemeData(primaryColor: Colors.blueAccent),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("My Flutter"),
-        ),
-        body: new Container(
-          child: new Column(
-            children: <Widget>[
-              new ListItem(
-                billNo: "ZY112222333",
-                billName: "十三供应商",
-                time: "2018-09-30",
-                statusText: "已验收",
-                status: Status.done,
-              ),
-              new ListItem(
-                billNo: "ZY112222333",
-                billName: "十三供应商",
-                time: "2018-09-30",
-                statusText: "已成功",
-                status: Status.success,
-              ),
-              new ListItem(
-                billNo: "ZY112222333",
-                billName: "十三供应商",
-                time: "2018-09-30",
-                statusText: "未完成",
-                status: Status.undone,
-              )
-            ],
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tabbed AppBar'),
+            bottom: TabBar(
+              isScrollable: true,
+              indicator: new ShapeDecoration(shape: new Border.all(color: Colors.redAccent, width: 1.0)),
+              indicatorSize: TabBarIndicatorSize.tab,
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
           ),
-          decoration: new BoxDecoration(
-            color: new Color(0xeeeeeeee)
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
           ),
         ),
       ),
@@ -53,3 +39,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Choice {
+  const Choice({ this.title, this.icon });
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = <Choice>[
+  Choice(title: 'CAR', icon: Icons.directions_car),
+  Choice(title: 'BICYCLE', icon: Icons.directions_bike),
+  Choice(title: 'BOAT', icon: Icons.directions_boat),
+  Choice(title: 'BUS', icon: Icons.directions_bus),
+  Choice(title: 'TRAIN', icon: Icons.directions_railway),
+  Choice(title: 'WALK', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({ Key key, this.choice }) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(TabbedAppBarSample());
+}
+
+/*
+Sample Catalog
+
+Title: Tabbed AppBar
+
+Summary: An AppBar with a TabBar for navigating pages just below it.
+
+Description:
+A TabBar can be used to navigate among the pages displayed in a TabBarView.
+Although a TabBar is an ordinary widget that can appear anywhere, it's most often
+included in the application's AppBar.
+
+Classes: AppBar, DefaultTabController, TabBar, Scaffold, TabBarView
+
+Sample: TabbedAppBarSample
+
+See also:
+  - The "Components-Tabs" section of the material design specification:
+    <https://material.io/go/design-tabs>
+*/
