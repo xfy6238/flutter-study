d0325b2b0957ca21924a30a2f673437be26c6d96
fanzkday <17612151214@163.com>
2018-10-22 21:15:21 +0800

    获取本地IP地址

--- lib/services/socket.dart
+++ lib/services/socket.dart
@@ -2,17 +2,14 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() async {
-  NetworkInterface.list().then((List<NetworkInterface> interfaces) {
-    final InternetAddress ip = interfaces[1].addresses[0];
-    Socket.connect(ip.address, 3200).then((Socket socket) {
-      socket.write("hello, server");
-      socket.listen((data) {
-        print(utf8.decode(data));
-      }, onDone: () {
-        print("done");
-      }, onError: (err) {
-        print(err);
-      });
+  Socket.connect(InternetAddress.loopbackIPv4, 3200).then((Socket socket) {
+    socket.write("hello, server");
+    socket.listen((data) {
+      print(utf8.decode(data));
+    }, onDone: () {
+      print("done");
+    }, onError: (err) {
+      print(err);
     });
   });
 }
